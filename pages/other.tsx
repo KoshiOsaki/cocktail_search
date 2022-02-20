f4f623a6308c2d3763efc03bdaeb6b667515a093
Koshi Osaki <oosaki@futurewoods.co.jpgit config --global user.email oosaki@futurewoods.co.jpgit config --global user.email oosaki@futurewoods.co.jp>
2022-02-20 13:30:00 +0900

    test

---
+++ pages/other.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+
+
+const Other: NextPage = () => {
+  return (
+    <div>
+      <p>other</p>
+    </div>
+  );
+};
+
+export default Other;
